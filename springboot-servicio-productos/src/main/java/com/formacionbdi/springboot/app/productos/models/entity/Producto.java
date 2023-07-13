3c502a4b5386ad420cdb0fd5381ae9d4d58ce816
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-13 07:37:00 +0200

    Creación del primer proyecto: productos

---
+++ springboot-servicio-productos/src/main/java/com/formacionbdi/springboot/app/productos/models/entity/Producto.java
@@ -0,0 +1,34 @@
+package com.formacionbdi.springboot.app.productos.models.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="productos")
+@Getter
+@Setter
+public class Producto implements Serializable{
+	
+	private static final long serialVersionUID = 1285454306356845809L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private Double precio;
+	
+	@Column(name="create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+}
---
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre, precio, create_at) values ('Panasonic', 800, NOW());
+INSERT INTO productos (nombre, precio, create_at) values ('SONY', 700, NOW());
+INSERT INTO productos (nombre, precio, create_at) values ('Apple', 1000, NOW() );
