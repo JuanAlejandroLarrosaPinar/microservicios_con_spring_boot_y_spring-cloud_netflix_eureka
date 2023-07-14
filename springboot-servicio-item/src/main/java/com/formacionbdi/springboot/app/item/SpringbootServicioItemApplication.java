3d6431ce919ef7feefcc4babbd0aa4c2fcba3ace
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-14 17:21:00 +0200

    Se implementa servidor eureka registrando los servicios item y productos en el mismo.

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplication.java
@@ -2,12 +2,15 @@ package com.formacionbdi.springboot.app.item;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(value="servicio-productos")
+//Al tener eureka se puede quitar
+//@RibbonClient(value="servicio-productos")
+@EnableEurekaClient
 public class SpringbootServicioItemApplication {
 
 	public static void main(String[] args) {
