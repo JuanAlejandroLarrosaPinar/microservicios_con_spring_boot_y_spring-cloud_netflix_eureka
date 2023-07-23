e42c07af2cd8f8f142b2a5ee660281bcd202d662
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-23 15:45:54 +0200

    Se modifican parámetros de Resilience4j para no utilizar los que vienen por defecto.

--- springboot-servicio-productos/src/main/resources/import.sql
+++ springboot-servicio-productos/src/main/resources/import.sql
@@ -2,3 +2,6 @@ INSERT INTO productos (nombre, precio, create_at) values ('Panasonic', 800, NOW(
 INSERT INTO productos (nombre, precio, create_at) values ('SONY', 700, NOW());
 INSERT INTO productos (nombre, precio, create_at) values ('Apple', 1000, NOW() );
 INSERT INTO productos (nombre, precio, create_at) values ('Mac', 1500, NOW() );
+INSERT INTO productos (nombre, precio, create_at) values ('Nike', 1500, NOW() );
+INSERT INTO productos (nombre, precio, create_at) values ('Puma', 1500, NOW() );
+INSERT INTO productos (nombre, precio, create_at) values ('Adidas', 1500, NOW() );
