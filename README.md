97d051ace6a7e1df4e375fe34b9989ab7e99b611
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-24 18:00:52 +0200

    Se implementa servidor spring-config.

--- README.md
+++ README.md
@@ -40,9 +40,16 @@
   - Circuit breaker se encarga de gestionar el hecho de que si hay más llamadas erroneas de lo normal, abra el cortocircuito y no permita más llamadas hasta pasado x tiempo
   - Timelimitter: sólo gestiona TimeOut.
 -Se puede implementar CircuitBreaker y Timelimitter en el servicio-gateway, para no ir haciéndolo servicio por servicio. Aunque a mi sólo me ha funcionado la parte de timelimitter y no la de circuitbreacker (el fallbackUri no lo hace correctamente).  
-  
 
-    
+- Config server:
+  - Se crea un proyecto nuevo
+  - Se indica cuál es la carpeta de configuración donde estarán todos los properties donde cada property tendrá nombre "nombre_instancia.properties".
+  - Se definen las configuraciones para cada proyecto
+  - Se accede a la ruta http://localhost:8888/servicio-item/default
+  - Se establece el servicio item como cliente de este proyecto. Además, la carpeta creada será un repositorio git donde estarán las properties de cada proyecto.
+  - Se pueden crear ficheros del tipo servicio-item-dev.properties o servicio-item-prod.properties para luego establecerse en la propiedad spring.profiles.active el perfil (dev o prod)
+  - Se pueden modificar properties en tiempo real con @RefreshScope en el controlador, pero hay que inyectar la dependencia en el pom del servicio cuyo controlador es el que se le ha metido la anotación y una vez que modificamos la propiedad, hay que hacer el commit y luego llamar a localhost:8005/actuator/refresh pero la petición tiene que ser de tipo POST. MUY IMPORTANTE.
+  - La carpeta config se puede subir a github y habría que modificar la property spring.cloud.config.server.git.uri (que es la que indica dónde está la configuración de nuestro servidor spring) estableciendo https://github.com/nombre_usuario/nombre_repositorio.git
   
 ### 18 Notas:
 - Desde la versión 2.4 de spring boot en adelante no es compatible con Ribbon.
--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/item/controllers/ItemController.java
@@ -1,13 +1,20 @@
 package com.formacionbdi.springboot.app.item.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -23,6 +30,7 @@ import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 //import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;//lo comentamos porque ya no vamos a utilizar hystrix, sino resilience4j
 
 
+@RefreshScope //nos permite actualizar las variables inyectadas sin reiniciar el servidor
 @RestController
 public class ItemController {
 	
@@ -30,10 +38,16 @@ public class ItemController {
 	
 	@Autowired
 	private CircuitBreakerFactory cbFactory;
+	
+	@Autowired
+	private Environment env;
 
 	@Autowired
 	@Qualifier(value = "serviceRestTemplate")
 	private ItemService itemService;
+	
+	@Value("${configuracion.texto}")
+	private String texto;
 	
 	@GetMapping("/listar")
 	public List<Item> listar(@RequestParam(name="nombre", required = false) String nombre, @RequestHeader(name="token-request", required = false) String tokenRequest){
@@ -95,5 +109,18 @@ public class ItemController {
 		producto.setPrecio(510.0);
 		item.setProducto(producto);
 		return CompletableFuture.supplyAsync(()->item);
+	}
+	
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto){
+		Map<String,String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		return new ResponseEntity<Map<String,String>>(json, HttpStatus.OK);
 	}
 }
