536ffdf0829ee204ca7fa7614805336688ee8cb0
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-28 23:53:02 +0200

    Se añade script para compilar todos los proyectos.

---
+++ compilar_todo.sh
@@ -0,0 +1,18 @@
+cd springboot-servicio-config-server;
+mvn clean compile;
+cd ../springboot-servicio-eureka-server;
+mvn clean compile;
+cd ../springboot-servicio-commons;
+mvn clean compile;
+cd ../springboot-servicio-productos;
+mvn clean compile;
+cd ../springboot-servicio-item;	
+mvn clean compile;
+cd ../springboot-servicio-usuarios-commons;
+mvn clean compile;
+cd ../springboot-servicio-usuarios;
+mvn clean compile;
+cd ../springboot-servicio-zuul-server;
+mvn clean compile;
+cd ../springboot-servicio-gateway-server;
+mvn clean compile;
