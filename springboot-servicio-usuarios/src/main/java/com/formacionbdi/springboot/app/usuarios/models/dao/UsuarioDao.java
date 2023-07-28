1b83cddd0bd12e88f537256ce8099e5b4d142b47
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-07-28 16:48:11 +0200

    Exportación de métodos del DAO a Endpoints.

--- springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ springboot-servicio-usuarios/src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,16 +1,21 @@
 package com.formacionbdi.springboot.app.usuarios.models.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.formacionbdi.springboot.app.usuarios.models.entity.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 
-	public Usuario findByUsernameAndEmail(String username, String email);
+	@RestResource(path="buscar-username")
+	public List<Usuario> findByUsername(@Param("nombre") String username);
 	
 	@Query("select u from Usuario u where u.username = ?1")
-	public Usuario obtenerPorUsername(String username);
+	public List<Usuario> obtenerPorUsername(String username);
 }
