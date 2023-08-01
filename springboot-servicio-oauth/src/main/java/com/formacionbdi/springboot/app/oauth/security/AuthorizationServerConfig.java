272c3b05d6035b65195fa06f109115ee2b2a9ec7
JuanAlejandro <alejandro.larrosa.pinar@gmail.com>
2023-08-01 18:03:15 +0200

    Establecemos properties de seguridad en el servidor central de configuración de spring.

--- springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/AuthorizationServerConfig.java
+++ springboot-servicio-oauth/src/main/java/com/formacionbdi/springboot/app/oauth/security/AuthorizationServerConfig.java
@@ -3,8 +3,10 @@ package com.formacionbdi.springboot.app.oauth.security;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -16,6 +18,7 @@ import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@RefreshScope
 @Configuration
 @EnableAuthorizationServer 
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
@@ -28,6 +31,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	
 	@Autowired
 	private InfoAdicionalToken infoAdicionalToken;
+	
+	@Autowired
+	private Environment env;
 	
 	@Override
 	//1-aquí configuramos el authenticationManager y local store (para que sea jwt) y el converter (componente)
@@ -50,7 +56,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("algun_codigo_secreto_aeiou");
+		tokenConverter.setSigningKey(env.getProperty("config.security.oauth.jwt.key"));
 		return tokenConverter;
 	}
 	
@@ -59,8 +65,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.
 		inMemory().
-		withClient("frontendapp").
-		secret( this.bcryptPasswordEncoder.encode("12345")).
+		withClient(env.getProperty("config.security.oauth.client.id")).
+		secret( this.bcryptPasswordEncoder.encode(env.getProperty("config.security.oauth.client.secret"))).
 		scopes("read","write"). //especificamos lo que puede hacer el usuario
 		authorizedGrantTypes("password", "refresh_token"). //cuando es con credenciales (cuando nuestros usuarios están en bbdd)
 		accessTokenValiditySeconds(3600).
--- springboot-servicio-oauth/src/test/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplicationTests.java
+++ springboot-servicio-oauth/src/test/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplicationTests.java
@@ -6,8 +6,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootServicioOauthApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
